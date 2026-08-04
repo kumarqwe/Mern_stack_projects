a40a57301f641dc6bb2d8db5a93d71652a32890e
prasanth <savarapuprasanthkumar2002@gmail.com>
2026-08-04 12:27:16 +0530

    try catch replace with WrapAsync

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const initatedata = require("./init/data.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsmate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"Views"));
@@ -32,8 +33,6 @@ async function main() {
     await mongoose.connect(MONGO_URL);
 }
 
-
-
 const initDB = async () =>  {
     await listing.deleteMany({});
     await listing.insertMany(initatedata.data);
@@ -45,89 +44,59 @@ initDB().catch((err) => console.error(err));
 
 // all listings data
 app.get("/listings", async (req,res)=> {
-    try {
         const listingdatas = await listing.find({});
-        res.render("./listing/index.ejs",{listingdatas});
-    } catch(err) {
-        res.send("something went wrong for index route");
-    }
-    
-})
+        res.render("./listing/index.ejs",{listingdatas});    
+});
 
 // show route for a single listing
 app.get("/listings/:id", async (req,res) => {
-    try {
-           const {id} = req.params;
+    const {id} = req.params;
     const listings = await listing.findById(id);
-    res.render("./listing/show.ejs", { listings });
-    } catch(err) {
-        res.send("something went wrong for edit route");
-    }
-    
+    res.render("./listing/show.ejs", { listings });   
 });
 
 // New Route
 app.get("/listing/new", (req,res) => {
-    try {
-        res.render("./listing/new.ejs");
-    } catch(err) {
-        res.send("something went wrong for new route");
-    }
-})
+    res.render("./listing/new.ejs");
+});
 
 //Create Route
-app.post("/listings", async (req,res, next) => {
-    try {
-        const newListing = new listing(req.body.listing);
-        await newListing.save();
-        res.redirect("/listings");
-    } catch(err) {
-        next(err);
-    }
+app.post("/listings", wrapAsync(async (req,res, next) => {
+    const newListing = new listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
 })
+);
 
 //Edit Route
 app.get("/listings/:id/edit", async (req,res, next) => {
-    try { 
-        const {id} = req.params;
+    const {id} = req.params;
     const listings = await listing.findById(id);
-    res.render("./listing/edit.ejs", { listing: listings });
-    } catch(err) {
-        next(err);
-    }
-    
+    res.render("./listing/edit.ejs", { listing: listings });    
 });
 
 //Update Route
-app.put("/listings/:id", async (req,res, next) => {
-    try {
-        let {id} = req.params;
-        await listing.findByIdAndUpdate(id, {...req.body.listing});
-        res.redirect(`/listings/${id}`);
-    } catch(err) {
-        next(err);
-    }
-});
+app.put("/listings/:id", wrapAsync(async (req,res, next) => {
+    let {id} = req.params;
+    await listing.findByIdAndUpdate(id, {...req.body.listing});
+    res.redirect(`/listings/${id}`);
+})
+);
 
 // Delete Route
-app.delete("/listings/:id", async (req,res) =>
+app.delete("/listings/:id", wrapAsync(async (req,res) =>
 {
-    try {
         let {id} = req.params;
         let deletedlisting = await listing.findByIdAndDelete(id);
         console.log(deletedlisting);
         res.redirect("/listings");
-    } catch(err) {
-        res.send("something went wrong for delete route");
-    }
-});
+})
+);
 
 // Error handling middleware
 app.use((err,req, res, next) => {
     res.send("something went wrong");
 });
-
-
 
 // server listing on it 
 app.listen(8080, () => {
