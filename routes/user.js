9311ebcd5a24bd674ca42e7e76e8890db489dfa2
PrashanthKumar Savarapu <a223947@avivagroup.com>
2026-09-07 16:49:16 +0530

    post signup

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
+const User = require("../Models/user.js");
+const wrapAsync = require("../utils/wrapAsync");
 
 router.get("/signup", (req,res) => {
     res.render("./users/signup.ejs");
 });
 
+router.post("/signup", wrapAsync(async(req,res) => {
+    try {
+      let {username, email, password} = req.body;
+      const newUser = new User({email, username});
+      const registeredUser = await User.register(newUser, password);
+      console.log(registeredUser);
+      req.flash("success","welcome to Wanderlust");
+      res.redirect("/listings");
+    }
+    catch (e) {
+        req.flash("error", e.message);
+        res.redirect("/signup");
+    }
+    
+}))
 module.exports = router;
