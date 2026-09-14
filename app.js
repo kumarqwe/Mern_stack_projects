aac516d29a439c3a5e641c32fe239e9fd6c6247f
prasanth <savarapuprasanthkumar2002@gmail.com>
2026-09-14 22:29:09 +0530

    config .env with process.env

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+if(process.env.NODE_ENV !== "production") {
+    require("dotenv").config();
+}
+console.log(process.env.CLOUD_NAME);
 const mongoose = require("mongoose");
 const listing = require("./Models/listing.js");
 const express = require("express");
